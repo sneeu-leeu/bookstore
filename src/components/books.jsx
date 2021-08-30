8fdcc9a6d32afc1347068905a3ffc96618eb0a47
sneeu-leeu <sa070197@gmail.com>
2021-08-30 15:22:33 +0200

    Add input buttons and book loist

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,7 +1,33 @@
+const books = [{
+  title: 'First Book',
+  author: 'Author1',
+  id: 1,
+}];
+
 export default function Books() {
   return (
     <div>
       <h3>All Books</h3>
+      <div>
+        <ul>
+          { books.map((book) => (
+            <li key={book.id}>
+              <span>{book.title}</span>
+              <button id={book.id} type="button">Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <form>
+        <h4>Add New Book</h4>
+        <input placeholder="Title" />
+        <br />
+        <br />
+        <input placeholder="Author" />
+        <br />
+        <br />
+        <button type="button">Add Book</button>
+      </form>
     </div>
   );
 }
